43419dd3e7056a14de8e4d61d670d0ba219b2fda
Pasqg <pasquale.giovenale@gmail.com>
2024-04-21 22:22:06 +0200

    Add 16K color map
    For better quality, let's use 16K color map (scaled down from original
    NASA's version - 21K).

--- defaults/src/main/java/org/pasqg/planetgenerator/defaults/Defaults.java
+++ defaults/src/main/java/org/pasqg/planetgenerator/defaults/Defaults.java
@@ -7,6 +7,6 @@ public enum Defaults {
     public static final String PLANETS_BLEND = Defaults.class.getResource("/planets.blend").getPath();
     public static final String BLENDER_SCRIPT = Defaults.class.getResource("/planet_render.py").getPath();
     public static final String HEIGHT_MAP = Defaults.class.getResource("/height_map_16K.png").getPath();
-    public static final String COLOR_MAP = Defaults.class.getResource("/default_color_map.tif").getPath();
+    public static final String COLOR_MAP = Defaults.class.getResource("/worldmap_16K.png").getPath();
     public static final String CLOUDS_MAP = Defaults.class.getResource("/default_clouds_map.tif").getPath();
 }
--- renderer/src/main/java/org/pasqg/planetgenerator/renderer/TextureScaler.java
+++ renderer/src/main/java/org/pasqg/planetgenerator/renderer/TextureScaler.java
@@ -10,11 +10,10 @@ import java.net.URL;
 public enum TextureScaler {
     ;
 
-    public static void main(String args[]) throws IOException {
-        String texturePath = Defaults.class.getResource("/height_map_21K.png").getPath();
+    public static void main(String[] args) throws IOException {
+        String texturePath = Defaults.class.getResource("/worldmap.jpg").getPath();
         BufferedImage image = ImageUtils.readFromFile(texturePath);
         BufferedImage scaledImage = ImageUtils.scale(image, 16384, 8192);
-        String resourcesDir = texturePath.substring(0, texturePath.lastIndexOf('/'));
-        ImageUtils.writeToFile(scaledImage, "./defaults/src/main/resources/height_map_16K.png", "png");
+        ImageUtils.writeToFile(scaledImage, "./defaults/src/main/resources/worldmap_16K.png", "png");
     }
 }
